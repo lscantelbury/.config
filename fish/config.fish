5d9d7b2b81e6f081394ecf99d9b5b9d57820faaf
lscantelbury <lhsda.snf20@uea.edu.br>
2022-06-24 17:11:12 -0400

    Updating bspwm and alacritty configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
-function fish_prompt
-    powerline-shell --shell bare $status
-end
 if status is-interactive
     # Commands to run in interactive sessions can go here
     neofetch
     set -U fish_greeting "🐟"
     alias emacs="emacsclient -c -a 'emacs'"
+end
+
+function fish_prompt
+    powerline-shell --shell bare $status
 end
